d1c62432a6e31992fde2133a8280f3280856ef3f
TerrformAutoGeneration <wb-wyh625345@alibaba-inc.com>
2020-04-09 19:46:48 +0800

    test

--- alicloud/resource_alicloud_cen_flowlog.go
+++ alicloud/resource_alicloud_cen_flowlog.go
@@ -1,13 +1,12 @@
 package alicloud
 
 import (
-	"time"
-
 	"github.com/aliyun/alibaba-cloud-sdk-go/services/cbn"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	"github.com/terraform-providers/terraform-provider-alicloud/alicloud/connectivity"
+	"time"
 )
 
 func resourceAlicloudCenFlowlog() *schema.Resource {
